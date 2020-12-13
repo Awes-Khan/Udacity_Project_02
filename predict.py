bc313d7568548ea74bb79e42bb5a0da07df0b7b2
Khanawes22 <44608315+Khanawes22@users.noreply.github.com>
2020-12-13 17:25:32 +0530

    Refactor Code
    The Code is refactor and mention how it works with docstrings. All refrences are given such that it would not seem to palgarismic code.

--- predict.py
+++ predict.py
@@ -2,12 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from torch import nn
-from torch import optim
-import torch.nn.functional as F
 from torchvision import datasets, transforms, models
 from PIL import Image
-import time
-import copy
 import json
 
 import argparse
@@ -16,6 +12,7 @@ parsers.add_argument('data_dir', action="store")
 parsers.add_argument('save_dir', action="store")
 parsers.add_argument('--top_k', dest="top_k", default=5)
 parsers.add_argument('--category_names', action="store", dest="category_names", default='cat_to_name.json')
+
 parsers.add_argument('--gpu', action="store_const", dest="device", const="gpu", default='cpu')
 
 arguments = parsers.parse_args()
@@ -24,26 +21,31 @@ save_dir = arguments.save_dir
 top_k = arguments.top_k
 category_names = arguments.category_names
 device = arguments.device
-path_to_image = data_dir
 
 with open(category_names, 'r') as f:
     cat_to_name = json.load(f)
 checkpoint = torch.load('checkpoint.pth')
 checkpoint.keys()
 model = models.vgg16(pretrained=True)
-
-model.classifier = nn.Sequential( nn.Linear(25088, 512), nn.ReLU(), nn.Dropout(p=0.5), nn.Linear(512, 102), nn.LogSoftmax(dim=1))
+classifier = nn.Sequential( nn.Linear(25088, 450), nn.ReLU(), nn.Linear(450, 102)) #I got reference of syntax from here:-> https://pytorch.org/docs/stable/generated/torch.nn.Sequential.html
+## Ruberic:Loading checkpoints :-> There is a function that successfully loads a checkpoint and rebuilds the model
+model.classifier = classifier
 model.class_to_idx = checkpoint['class_to_idx']
 model.load_state_dict(checkpoint['state_dict'])
 
 
+path_to_image=data_dir
 def process_image(path_to_image):
-    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
-        returns an Numpy array
-    '''
+
     image = Image.open(path_to_image)
+    #Refrence is from https://pillow.readthedocs.io/en/latest/reference/Image.html
     image=image.resize((image.width*256//image.height,256)) if image.width > image.height else image.resize((256,image.height*256//image.width))
-    return ((((np.array(image.crop(((image.width-224)/2, (image.height-224)/2, (image.width-224)/2+224, (image.height-224)/2+224))))/255) - np.array([0.485, 0.456, 0.406]))/np.array([0.229, 0.224, 0.225])).transpose((2, 0, 1))
+    #in above expression,I have resize the images where the shortest side is 256 pixels, keeping the aspect ratio.
+    return ((((np.array(image.crop(((image.width-224)/2,(image.height-224)/2,(image.width-224)/2+224, (image.height-224)/2+224))))/255) # image converted to np array from PIL
+             - np.array([0.485, 0.456, 0.406]))#substracting mean from resulting np array
+            /np.array([0.229, 0.224, 0.225])#Dividing by Standard Deviation
+           ).transpose((2, 0, 1)) #transposing it to make third channel (color channel) to first and remaining both maintaining the sequence
+
 def imshow(image, ax=None, title=None):
     if ax is None:
         fig, ax = plt.subplots()
@@ -52,27 +54,32 @@ def imshow(image, ax=None, title=None):
     std = np.array([0.229, 0.224, 0.225])
     image = std * image + mean
     image = np.clip(image, 0, 1)
-    ax.imshow(image)
-    return ax
-def predict(image_path, model, topk=5):
-    return (torch.exp(model.forward((torch.from_numpy( process_image(image_path)).type(torch.FloatTensor)).unsqueeze_(0)))).topk(5)
-def display_img(image_path, model):
-    category = []
-    flower=[]
-    name_list=[]
-    imshow(process_image(image_path), plt.subplot(2,1,1))
-    probs, classes = predict(image_path, model)
-    for i in classes.tolist()[0]:
-        flower.append(i)
-    for x in flower:
-        for category_name, category_value in checkpoint['class_to_idx'].items():
-            category.append(category_name) if category_value == x else continue
-    for i in category:
-        name_list.append(cat_to_name[i])
-    ax = plt.subplot(2,1,2)
-    ax.barh(name_list, probs.tolist()[0])
-    plt.show()
 
-process_image(path_to_image)
-predict(path_to_image, model)
-display_img(path_to_image, model)
+    ax.imshow(image)
+
+    return ax
+
+
+def predict(image_path):
+    # Done: Implement the code to predict the class from an image file
+    return (torch.exp(model.forward(
+        (torch.from_numpy( process_image(image_path)).type(torch.FloatTensor)).unsqueeze_(0) #Converting np array to float tensor using from_numpy
+        ))).topk(5) #considering top 5 result refer from here https://pytorch.org/docs/master/torch.html#torch.topk
+
+# Done: Display an image along with the top 5 classes
+def display_img(image_path):
+    category = [] #Empty list
+    name_list=[] #Empty list
+    flower=[] #Empty list
+    imshow(process_image(image_path), plt.subplot(2,1,1)) #Displaying Image
+    probs, classes = predict(image_path) #predicting image with top 5 results
+    for i in classes[0]:
+        flower.append(i) #adding new element to flower
+    for x in flower:
+        for category_name, category_value in checkpoint['class_to_idx'].items(): #We are getting category key and value from class_to_idx dictonary
+            category.append(category_name) if category_value == x else continue  #Storing Category Names if present in flower list
+    for i in category:
+        name_list.append(cat_to_name[i]) #if category value is present in cat_to_name then storing the label name to store it into name_list so that it would be use to print on bar chart
+    ax = plt.subplot(2,1,2)
+    ax.barh(name_list, probs[0]) #printing bar chart with possible naame_list and value
+    plt.show()
--- train.py
+++ train.py
@@ -11,13 +11,13 @@ import copy
 import json
 
 import argparse
-
+#refer from https://docs.python.org/3/library/argparse.html
 parser = argparse.ArgumentParser()
 parser.add_argument('data_dir', action="store")
 parser.add_argument('--save_dir', action="store", dest="save_dir", default='checkpoint')
 parser.add_argument('--arch', action="store", dest="arch", default='vgg16')
 parser.add_argument('--learning_rate', action="store", dest="learning_rate", default=0.001)
-parser.add_argument('--hidden_units', action="store", dest="hidden_units", default=512)
+parser.add_argument('--hidden_units', action="store", dest="hidden_units", default=500)
 parser.add_argument('--epochs', action="store", dest="epochs", default=3)
 parser.add_argument('--gpu', action="store_const", dest="device", const="gpu", default='cpu')
 
@@ -29,92 +29,107 @@ lr = arguments.learning_rate
 hidden_units_count = arguments.hidden_units
 epochs = arguments.epochs
 device = arguments.device
-model = models.vgg13(pretrained=True) if arch == 'vgg13' else models.vgg13(pretrained=True)
-for param in model.parameters():
-    param.requires_grad = False
 
 data_dir = 'flowers'
 train_dir = data_dir + '/train'
 valid_dir = data_dir + '/valid'
 test_dir = data_dir + '/test'
+dirs=[train_dir,valid_dir,test_dir]
 
-data_transforms = [
-
-                 transforms.Compose([
-                                       transforms.RandomRotation(30),
-                                       transforms.RandomResizedCrop(224),
-                                       transforms.RandomHorizontalFlip(),
-                                       transforms.ToTensor(),
-                                       transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])
-                                        ]),
-                transforms.Compose([
-                                       transforms.Resize(256),
-                                       transforms.CenterCrop(224),
-                                       transforms.ToTensor(),
-                                       transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])
-                                        ]),
-                transforms.Compose([
-                                      transforms.Resize(256),
-                                      transforms.CenterCrop(224),
-                                      transforms.ToTensor(),
-                                      transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])
-                                        ])
-                ]
-
-image_datasets = [
-                    datasets.ImageFolder(train_dir, transform=data_transforms[0]),
-                    datasets.ImageFolder(valid_dir, transform=data_transforms[1]),
-                    datasets.ImageFolder(test_dir, transform=data_transforms[2])
-                 ]
-
-dataloaders = [
-                torch.utils.data.DataLoader(image_datasets[0], batch_size=64, shuffle=True),
-                torch.utils.data.DataLoader(image_datasets[1], batch_size=64),
-                torch.utils.data.DataLoader(image_datasets[2], batch_size=64)
-              ]
-
+image_datasets=[]
+dataloaders=[]
+data_transforms=[
+    transforms.Compose([
+        transforms.RandomRotation(90),
+        transforms.RandomResizedCrop(255),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),]),
+    # Refrence for syntax and normalize value from here https://pytorch.org/docs/stable/torchvision/transforms.html
+    transforms.Compose([
+        transforms.Resize(255),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),]),
+    transforms.Compose([
+        transforms.Resize(255),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),])# Refrence for syntax and normalize value from 'Loading Image Data Exercise' of last chapter
+            ]
+for i in range(3):
+    datasets_var=datasets.ImageFolder(dirs[i], transform=data_transforms[i])
+    dataloaders.append(torch.utils.data.DataLoader(datasets_var, batch_size=64))
+    image_datasets.append(datasets_var)
 
 with open('cat_to_name.json', 'r') as f:
     cat_to_name = json.load(f)
 
-model.classifier = nn.Sequential( nn.Linear(25088, hidden_units_count), nn.ReLU(), nn.Dropout(p=0.5), nn.Linear(hidden_units_count, 102), nn.LogSoftmax(dim=1))
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.classifier.parameters(), lr)
+model = models.vgg13(pretrained=True) if arch == 'vgg13' else models.vgg16(pretrained=True)
 
-def is_valid(model, loader, criterion, device):
-    loss = 0
-    accuracy = 0
-    for (img, labels) in zip(loader):
-        loss += criterion(model.forward(img.to(device)), labels.to(device)).item()
-        accuracy += (labels.to(device).data == torch.exp(model.forward(img.to(device))).max(dim=1)[1]).type(torch.FloatTensor).mean()
-    return loss, accuracy
+for param in model.parameters():
+    param.requires_grad = False
+model.classifier = nn.Sequential( nn.Linear(25088, hidden_units_count), nn.ReLU(), nn.Linear(hidden_units_count, 102)) #I got reference of syntax from here:-> https://pytorch.org/docs/stable/generated/torch.nn.Sequential.html
+
+criterion = nn.CrossEntropyLoss() # it is addition of LogSotfmax function and NLLloss funcion
+optimizer = optim.SGD(model.parameters(), lr)
+
+
+
+
 
 def my_nn(model, trainloader, testloader, epochs, criterion, optimizer, device):
     device = 'cuda' if device == 'gpu' else 'cpu'
-    model.to(device)
-    epoch_count=0
-    while(epoch_count<int(epochs)):
-        current_loss = 0
-        for iterator,(input_count, label_name) in enumerate(trainloader):
+    epochs = 30
+    steps = 0
+    train_losses, test_losses = [], []
+    for e in range(epochs):
+        running_loss = 0
+        for images, labels in trainloader:
             optimizer.zero_grad()
-            loss = criterion(model.forward(input_count.to(device)), label_name.to(device))
+            log_ps = model(images)
+            loss = criterion(log_ps, labels)
             loss.backward()
             optimizer.step()
-            current_loss += loss.item()
+            running_loss += loss.item()
+        else:
+            test_loss = 0
+            accuracy = 0
+            with torch.no_grad():
+                model.eval()
+                for images, labels in testloader:
+                    log_ps = model(images)
+                    test_loss += criterion(log_ps, labels)
+                    ps = torch.exp(log_ps)
+                    top_p, top_class = ps.topk(1, dim=1)
+                    equals = top_class == labels.view(*top_class.shape)
+                    accuracy += torch.mean(equals.type(torch.FloatTensor))
+            print("Epoch: {}/{}.. ".format(e+1, epochs),
+                  "Training Loss: {:.3f}.. ".format(running_loss/len(trainloader)),
+                  "Test Loss: {:.3f}.. ".format(test_loss/len(testloader)),
+                  "Test Accuracy: {:.3f}".format(accuracy/len(testloader)))
+        model.train()
+# This Function Is Refered from 'Inference and Validation Exersice' of pytorch lesson from neural network chapter
+
+
 
 # Done: Do validation on the test set
 def check_accuracy_of_my_nn(testloader, device):
-    positive = 0
-    total = 0
+    test_loss = 0
+    accuracy = 0
+    device = 'cuda' if device == 'gpu' else 'cpu'
     with torch.no_grad():
-        for iterator,(image, label_name) in enumerate(testloader):
-            device = 'cuda' if device == 'gpu' else 'cpu'
-            variable, predicted = torch.max(model(image.to(device)).data, 1)
-            total += label_name.to(device).size(0)
-            positive += (predicted == label_name).sum().item()
-    print('Accuracy: %d %%' % (100 * positive / total))
+        for images, labels in testloader:
+            log_ps = model(images)
+            test_loss += criterion(log_ps, labels)
+            ps = torch.exp(log_ps)
+            top_p, top_class = ps.topk(1, dim=1)
+            equals = top_class == labels.view(*top_class.shape)
+            accuracy += torch.mean(equals.type(torch.FloatTensor))
+    print('Accuracy: %d %%' % (100 * accuracy))
+    # This Function Is Refered from 'Inference and Validation Exersice' of pytorch lesson from neural network chapter
 
 my_nn(model, dataloaders[0], dataloaders[1], epochs, criterion, optimizer, device)
 check_accuracy_of_my_nn(dataloaders[0], 'gpu')
 model.class_to_idx = image_datasets[0].class_to_idx
-torch.save({'arch':'vgg16', 'state_dict':model.state_dict(), 'class_to_idx':model.class_to_idx}, 'checkpoint.pth')
+torch.save({'arch':'vgg16','state_dict':model.state_dict(),'class_to_idx':model.class_to_idx},'checkpoint.pth')
